f315846f979bbb40ea168f883b9ffe9289eebd78
mohammed <lovelyyasar135@gmail.com>
2025-12-30 22:07:02 +0530

    error fix

--- movie-rating-app/client/src/services/api.js
+++ movie-rating-app/client/src/services/api.js
@@ -1,4 +1,9 @@
-const API_BASE_URL = (import.meta.env.VITE_API_URL || 'http://localhost:5000/api').replace(/\/$/, '');
+let apiBase = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+apiBase = apiBase.replace(/\/$/, '');
+if (!apiBase.endsWith('/api')) {
+  apiBase += '/api';
+}
+const API_BASE_URL = apiBase;
 
 /**
  * Generic API request handler
